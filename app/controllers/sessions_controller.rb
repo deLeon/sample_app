fbafefe4a8a604b3049a85a3c039eb52f83561cd
Felix Santiago <deleonventures@gmail.com>
2011-09-30 21:30:46 -0400

    Done with user edit/update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
       render 'new'
     else
       sign_in user
-      redirect_to user
+      redirect_back_or user
     end
   end
 
@@ -22,5 +22,5 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_path
   end
-
 end
+
